419267b2f033f803c6a7c2485c2d7b8bcd9cfeb6
MartyMinaic <53369178+MartyMinaic@users.noreply.github.com>
2019-07-27 19:51:20 +0530

    Update multiples.java

--- multiples.java
+++ multiples.java
@@ -1,58 +1,19 @@
-     /*If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
-    
-         * Find the sum of all the multiples of 3 or 5 below 1000.
-         * */
+/*If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+Find the sum of all the multiples of 3 or 5 below 1000.*/
 class multiples
 {
     public static void main(String args[])
     {
-        //total number of multiples of 3 and 5
-        int mt[] = new int[532];
-        boolean k=true;
-        int c=0,d=1;
-        //c is counter variable
-        //while k is true
-        while(k)
+        int s=0;
+        for(int a=1; a<1000; a++)
         {
-            //Takes out multiples of 3
-            if(3*d>1000)
+            if(a%3==0 || a%5==0)
             {
-                k=false;
-            }
-            else
-            {
-                mt[c]=3*d;
-                c++;
-                d++;
+                s=s+a;
+                System.out.println(a);
             }
         }
-        k=true;
-        d=1;
-        while(k)
-        {
-            //Takes out multiples of 5
-            if(5*d>=1000)
-            {
-                k=false;
-            }
-            else
-            {
-                mt[c]=5*d;
-                c++;
-                d++;
-            }
-        }
-        int sum=0;
-        for(int a=1; a<=1000; a++)
-        {
-            for(int i=0; i<mt.length; i++)
-            {
-                if(mt[i]==a)
-                {
-                    sum=sum+a;
-                }
-            }
-        }
-        System.out.print("Sum ="+sum);
+        System.out.print("Sum = "+s);
     }
 }
