310830e1868c39fc5769c26f8e05a1950450e675
MartyMinaic <53369178+MartyMinaic@users.noreply.github.com>
2019-07-27 19:53:34 +0530

    Update multiples.java

--- multiples.java
+++ multiples.java
@@ -6,8 +6,9 @@ class multiples
     public static void main(String args[])
     {
         int s=0;
-        for(int a=1; a<1000; a++)
+        for(int a=1; a<=1000; a++)
         {
+            //Checking if the number is divisible by 3 or 5 or not. If so then adding them to a varriable s
             if(a%3==0 || a%5==0)
             {
                 s=s+a;
