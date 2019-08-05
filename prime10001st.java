a3674ff92c01de33245e53bcdeb0abf384c1512c
Rohan Verma <53369178+MartyMinaic@users.noreply.github.com>
2019-08-05 14:11:12 +0530

    Update prime10001st.java

--- prime10001st.java
+++ prime10001st.java
@@ -6,29 +6,27 @@ class prime10001st
     public static void main(String args[])
     {
         prime10001st ob = new prime10001st();
-        int ct=0,n=1;
-        while(ct<10001)
+        int ct=1,n=1;
+        boolean k=false;
+        while(ct<10000)
         {
-            n++;
-            if(ob.isPrime(n))
+            n++;            
+            k=true;
+            if(n < 2) 
+            k=false;
+            if(n == 2 || n == 3) 
+            k=true;
+            if(n%2 == 0 || n%3 == 0) 
+            k=false;
+            long sqrtN = (long)Math.sqrt(n)+1;
+            for(long i = 6L; i <= sqrtN; i += 6) 
+            {
+                if(n%(i-1) == 0 || n%(i+1) == 0) 
+                k=false;
+            } 
+            if(k)
             ct++;
         }
         System.out.println(n);
-    }
-    boolean isPrime(long n)
-    {
-        if(n < 2) 
-        return false;
-        if(n == 2 || n == 3) 
-        return true;
-        if(n%2 == 0 || n%3 == 0) 
-        return false;
-        long sqrtN = (long)Math.sqrt(n)+1;
-        for(long i = 6L; i <= sqrtN; i += 6) 
-        {
-            if(n%(i-1) == 0 || n%(i+1) == 0) 
-            return false;
-        } 
-        return true;
     }
 }
