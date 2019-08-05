227e6382a6f3c06fcd4fa1cbfc4dbe71414b4f66
Rohan Verma <53369178+MartyMinaic@users.noreply.github.com>
2019-08-05 13:22:18 +0530

    Update sumSquareDifference.java

--- sumSquareDifference.java
+++ sumSquareDifference.java
@@ -4,7 +4,7 @@
 The square of the sum of the first ten natural numbers is,
 
 (1 + 2 + ... + 10)2 = 552 = 3025
-Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is 3025 - 385 = 2640.
+Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is 3025 − 385 = 2640.
 
 Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.*/
 class sumSquareDifference
@@ -15,15 +15,11 @@ class sumSquareDifference
     public static void main(String args[])
     {
         int n1=0,n2=0;
-        for(int a=1; a<=100; a++)
-        {
-            n1=n1+(int)Math.pow(a,2);
-        }
-        for(int a=1; a<=100; a++)
-        {
-            n2=n2+a;
-        }
-        n2=(int)Math.pow(n2,2);
+        /* sumation of series formula sum of squares = n*(n+1)*(2n+1)/6*/
+        n1=(int)((100*(100+1)*(2*100+1))/6.0);
+        /* summation of n terms = n*(n+1)/2*/
+        n2=(int)((100*(100+1))/2.0);
+        n2=n2*n2;
         int ans=n2-n1;
         System.out.print("Answer = "+ans);
     }
