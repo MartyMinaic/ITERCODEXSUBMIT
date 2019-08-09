00a26203cdb11324c6928dd49de77a7fe5d17f76
Rohan Verma <53369178+MartyMinaic@users.noreply.github.com>
2019-08-09 15:53:56 +0530

    Update and rename prime10001st.java to question7.java

--- question7.java
+++ question7.java
@@ -1,11 +1,11 @@
 /*By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
 
 What is the 10 001st prime number?*/
-class prime10001st
+class question7
 {
     public static void main(String args[])
     {
-        prime10001st ob = new prime10001st();
+        question7 ob = new question7();
         int ct=1,n=1;
         boolean k=false;
         while(ct<10000)
