5fc5d185521e5b1b6e51438ad34dfefef4d9de2b
Rohan Verma <53369178+MartyMinaic@users.noreply.github.com>
2019-08-09 15:58:04 +0530

    Update and rename productInSeries.java to question8.java

--- question8.java
+++ question8.java
@@ -22,7 +22,7 @@
 71636269561882670428252483600823257530420752963450
 
 Find the thirteen adjacent digits in the 1000-digit number that have the greatest product. What is the value of this product?*/
-class productInSeries
+class question8
 {
     public static void main(String args[])
     {
