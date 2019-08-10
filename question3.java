e87e7db00f84e8da83befe1bea2359fd68e2ed50
Rohan Verma <53369178+MartyMinaic@users.noreply.github.com>
2019-08-10 20:16:24 +0530

    Update question3.java

--- question3.java
+++ question3.java
@@ -3,21 +3,16 @@ class question3
     public static void main(String Args[])
     {
         long s=600851475143l;
-        int t=0;
-        for(int a=2; a<=s; a++)
+        long s_copy=s,ans=0;    
+        for(int a=2;a<=Math.sqrt(s_copy);a++)
         {
-            while(true)
-            {
-                if(s%a==0)
-                {
-                    t=a;
-                    s=s/a;
-                }
-                else
-                break;
-            }
-        }
-        System.out.print(t);
+            while((s%a)==0)
+            {   
+                ans=a;
+                s=s/a;    
+            }    
+        }    
+        System.out.print(ans);
     }
 }
             
